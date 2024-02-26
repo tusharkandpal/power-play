87b853d356222380c5d31fc1d030f18e0a6a5c26
Tushar Kandpal <kandpal.tushar@gmail.com>
2024-02-26 15:51:49 +0530

    CSS changes

--- src/component/must-watch/MustWatch.jsx
+++ src/component/must-watch/MustWatch.jsx
@@ -5,7 +5,7 @@ export const MustWatch = ({ videoId }) => {
   const { videos } = useSelector((store) => store.videoTimeline);
 
   return (
-    <div className="min-w-fit overflow-y-auto no-scrollbar mt-4">
+    <div className="min-w-fit overflow-y-auto no-scrollbar m-5">
       <h2 className="text-4xl mb-2">Must Watch</h2>
       <div className="flex flex-col gap-3">{videos.map(
         (video) =>
